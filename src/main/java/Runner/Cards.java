f645b3de0ab8629c9710f527742c0423c5aa28ba
Witold Buczek <winsent0072@gmail.com>
2024-06-04 20:24:35 +0200

    Duże zmiany w klasie PlayersTeam i kosmetyczne w klasie Cards

--- src/main/java/Runner/Cards.java
+++ src/main/java/Runner/Cards.java
@@ -3,43 +3,44 @@ package Runner;
 import java.util.ArrayList;
 
 public class Cards {
-    Draw draw = new Draw();
-    ArrayList<Integer> bramkarz = draw.drawKeeper();
-    ArrayList<Integer> obronca1 = draw.drawDefender();
-    ArrayList<Integer> obronca2 = draw.drawDefender();
-    ArrayList<Integer> obronca3 = draw.drawDefender();
-    ArrayList<Integer> obronca4 = draw.drawDefender();
-    ArrayList<Integer> pomocnik1 = draw.drawMidfielder();
-    ArrayList<Integer> pomocnik2 = draw.drawMidfielder();
-    ArrayList<Integer> pomocnik3 = draw.drawMidfielder();
-    ArrayList<Integer> pomocnik4 = draw.drawMidfielder();
-    ArrayList<Integer> napastnik1 = draw.drawAttacker();
-    ArrayList<Integer> napastnik2 = draw.drawAttacker();
+    private Draw draw = new Draw();
+    private ArrayList<ArrayList<Integer>> playerCards;
+    private ArrayList<ArrayList<Integer>> computerCards;
+
     public Cards() {
-        ArrayList<ArrayList<Integer>> playerCards = new ArrayList<>();
-        playerCards.add(bramkarz);
-        playerCards.add(obronca1);
-        playerCards.add(obronca2);
-        playerCards.add(obronca3);
-        playerCards.add(obronca4);
-        playerCards.add(pomocnik1);
-        playerCards.add(pomocnik2);
-        playerCards.add(pomocnik3);
-        playerCards.add(pomocnik4);
-        playerCards.add(napastnik1);
-        playerCards.add(napastnik2);
-        ArrayList<ArrayList<Integer>> computerCards = new ArrayList<>();
-        computerCards.add(bramkarz);
-        computerCards.add(obronca1);
-        computerCards.add(obronca2);
-        computerCards.add(obronca3);
-        computerCards.add(obronca4);
-        computerCards.add(pomocnik1);
-        computerCards.add(pomocnik2);
-        computerCards.add(pomocnik3);
-        computerCards.add(pomocnik4);
-        computerCards.add(napastnik1);
-        computerCards.add(napastnik2);
+        this.draw = new Draw();
+        this.playerCards = generateCards();
+        this.computerCards = generateCards();
+    }
+
+    public ArrayList<ArrayList<Integer>> generateCards() {  //metoda typu "lista w liście", bo tworzę listę składającą się z list
+        ArrayList<ArrayList<Integer>> cards = new ArrayList<>();    //tworzę nową listę z typem "ArrayList<Integer>", ale tak naprawdę to jest tam typ "Integer"
+        cards.add(draw.drawKeeper());   //losowanie bramkarza
+        for (int i = 0; i < 4; i++) {   //losowanie 4 obrońców
+            cards.add(draw.drawDefender());
+        }
+        for (int i = 0; i < 4; i++) {   //losowanie 4 pomocników
+            cards.add(draw.drawMidfielder());
+        }
+        for (int i = 0; i < 2; i++) {   //losowanie 2 napastników
+            cards.add(draw.drawAttacker());
+        }
+        return cards;   //zwraca listę 11 piłkarzy
+    }
+
+    public ArrayList<ArrayList<Integer>> getPlayerCards() {
+        return playerCards;
+    }
+
+    public ArrayList<ArrayList<Integer>> getComputerCards() {
+        return computerCards;
+    }
+    public void displayPlayerCards() {
+        System.out.println("Twoje karty piłkarzy:");
+        for (int i = 0; i < playerCards.size(); i++) {
+            ArrayList<Integer> card = playerCards.get(i);
+            System.out.println("Piłkarz " + (i+1) + ": " + card);
+        }
     }
 }
 
